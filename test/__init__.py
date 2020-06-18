884b7ae1fa3a9b30cf0547ed5c9d90567ebacbc4
Gabriela Sanchez-Diaz <gsdiaz21@gmail.com>
2020-06-18 13:26:50 -0400

    structure package

---
+++ test/__init__.py
@@ -0,0 +1,15 @@
+""" """
+
+
+# __all__ = [
+#     "BaseFanCI",
+#     "BaseOverlap",
+#     "APIGFanci",
+#     "APIGOverlap",
+# ]
+
+
+# from objective.base import BaseFanCI
+# from objective.base import BaseOverlap
+# from objective.apig import APIGFanCI
+# from objective.apig import APIGOverlap
